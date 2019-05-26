1c81b34fafa4f9b18d984382cef3d0b937acd578
amrods <11388389+amrods@users.noreply.github.com>
2019-05-27 08:33:13 +1000

    fixed bug in ackley

--- test_functions.jl
+++ test_functions.jl
@@ -3,7 +3,7 @@
 # Ackley
 function ackley(x; a = 20, b = 0.2, c = 2π)
     d = length(x)
-    return -a*exp(-b*sqrt(sum(x.^2)/d)) - exp(sum(cos.(c*xi) for xi in x)/d) + a
+    return -a*exp(-b*sqrt(sum(x.^2)/d)) - exp(sum(cos.(c*xi) for xi in x)/d) + a + exp(1)
 end
 
 # Booth
